0eaac67ea8f31f6af4fd959b3c3e05887b558710
Никита Кудряков <nikitakudrakov@MacBook-Pro-Nikita.local>
2019-02-08 09:14:04 +0300

    Initial Commit

---
+++ HockeyWithMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HockeyWithMe
+//
+//  Created by Никита Кудряков on 08/02/2019.
+//  Copyright © 2019 Никита Кудряков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
